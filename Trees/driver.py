77c3aa8dad436ef5be261407b38b6b47c9c60371
Vansh <vansharora2929@gmail.com>
2022-06-15 13:00:00 +0530

    driver.py

--- Trees/driver.py
+++ Trees/driver.py
@@ -16,15 +16,20 @@ def build():
     nn.right = build()
     return nn
 
-# Recursive
-def preOrder(root):
-    if root == None:
-        return
-
-    print(root.data)
-    preOrder(root.left)
-    preOrder(root.right)
-
+def PreOrder(root):
+    stck = []
+    ans = []
+    curr = root
+    first = 1
+    while curr or first or len(stck) > 0:
+        first = 0
+        while curr:
+            ans.append(curr.data)
+            stck.append(curr)
+            curr = curr.left
+        curr = stck.pop()
+        curr = curr.right
+    return ans
 
 root = build()
-preOrder(root)
+print(PreOrder(root))
